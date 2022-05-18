429fdec038fafc294d1d1efe4cde885781b65af3
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-05-18 10:37:41 +0100

    create formData function

---
+++ src/modules/formData.js
@@ -0,0 +1,15 @@
+
+
+//get the data from the form
+export const formData =(e)=> {
+    e.preventDefault();
+      var name =  document.querySelector('.name').value;
+      var score= document.querySelector('.score').value;
+      console.log(name);
+
+const obj = {
+    'name':name,
+    'score':score
+}
+postData(obj);
+}
